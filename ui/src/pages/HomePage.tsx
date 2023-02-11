c6a17e72a528c5d8969b15bae50aad0757eac457
himok <himok.pk@gmail.com>
2023-02-11 16:59:45 +0100

    Creating HomePage and NavBar

---
+++ ui/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = (() => {
+    return (
+        <div className="content">
+            NavBar
+        </div>
+    )
+
+});
+
+export default HomePage;
