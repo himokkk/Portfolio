3aac359a11cf783e40bf60a324099ec93ce63bc2
himok <himok.pk@gmail.com>
2023-02-11 17:50:24 +0100

    Adding basic navbar

--- ui/src/components/NavBar.tsx
+++ ui/src/components/NavBar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import '../css/basic.css';
+import '../css/navbar.css';
+
 const NavBar = (() => {
     return (
-        <div>
-            Page
+        <div className="navbar">
+           <div className="navbar-button float-left" id="nameButton">Paweł</div> 
+           <div className="navbar-button float-left" id="homeButton">Home</div> 
+           <div className="navbar-button float-left" id="aboutButton">About</div> 
+           <div className="navbar-button float-left" id="skillsButton">Skills</div> 
+           <div className="navbar-button float-left" id="contactButton">Contact</div>  
+           <div className="clear-both"/>                  
         </div>
     )
 });
