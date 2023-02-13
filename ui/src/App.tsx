7e14238575ae88c4649e85fe5751ab742c1c12f4
himok <himok.pk@gmail.com>
2023-02-13 21:27:59 +0100

    Creating Routes and chess page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,19 @@
+import{ HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
-
+import ChessPage from './pages/ChessPage';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <HomePage/>
+      <HashRouter>
+          <Routes>
+              <Route path='/' element={<div><NavBar/><HomePage/></div>}></Route>
+              <Route path='/chess' element={<div><NavBar/><ChessPage/></div>}></Route>
+          </Routes>
+        </HashRouter>
     </div>
   );
 }
---
+++ ui/src/pages/ChessPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ChessPage: React.FC = (() => {
+    return (
+        <div></div>
+    )
+
+})
+
+export default ChessPage;
