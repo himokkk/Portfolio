d480d37ec3a0a46236191d097638d5f17d7c597e
himok <himok.pk@gmail.com>
2023-02-20 09:54:26 +0100

    test

--- ui/src/components/TechStackComponent.tsx
+++ ui/src/components/TechStackComponent.tsx
@@ -14,15 +14,20 @@ import HTMLLogo from '../assets/techstack/html-logo.svg';
 import DjangoLogo from '../assets/techstack/django-logo.svg';
 import DockerLogo from '../assets/techstack/docker-logo.svg';
 import CppLogo from '../assets/techstack/cpp-logo.svg';
-import { constants } from 'fs/promises';
 
 interface Vector {
     x: number;
     y: number;
 }
 
+interface Ball {
+    x: number;
+    y: number;
+    radius: number;
+}
+
 const TechStackComponent = (() => {
-    function getRandomColor() {
+    function getRandomColor(): string {
         const hue = Math.floor(Math.random() * 361);
         const saturation = '70%';
         const lightness = '80%';
@@ -31,8 +36,8 @@ const TechStackComponent = (() => {
     }
     
     useEffect (() => {
-        let Elements = document.getElementsByClassName("randomColor") as HTMLCollectionOf<HTMLDivElement>;
-        Array.from(Elements).forEach((element: HTMLDivElement) => {
+        let elements = document.getElementsByClassName("randomColor") as HTMLCollectionOf<HTMLDivElement>;
+        Array.from(elements).forEach((element: HTMLDivElement) => {
             var bubbleElement = element.querySelector(".bubble") as HTMLDivElement;
             var secondChildElement1 = bubbleElement.querySelector("span:nth-child(1)")  as HTMLDivElement;
             var secondChildElement2 = bubbleElement.querySelector("span:nth-child(2)")  as HTMLDivElement;
@@ -43,116 +48,104 @@ const TechStackComponent = (() => {
         });
     }, []);
 
-    function randomSpawnPosition(centerPoint: Vector, jar_size: number, bubble_size: number)  {
+    function randomSpawnPosition(centerPoint: Vector, jar_size: number, bubble_size: number): Vector  {
         let ballPosition: Vector = {
-            x: Math.floor(Math.random() * (jar_size - bubble_size - 100)),
-            y: Math.floor(Math.random() * (jar_size - bubble_size - 100)) + jar_size / 10,
+            x: Math.floor(Math.random() * (jar_size*0.8 - bubble_size)),
+            y: Math.floor(Math.random() * (jar_size*0.8 - bubble_size)),
         }
         let distX = ballPosition.x - centerPoint.x - bubble_size/2;
         let distY = ballPosition.y - centerPoint.y - bubble_size/2;
+
         while(Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2)) >= jar_size / 2 - bubble_size / 2) {
-            let random = Math.random();
             ballPosition = {
-                x: Math.floor(random * (jar_size - bubble_size - 100)),
-                y: Math.floor(Math.random() * (jar_size - bubble_size - 100)) + jar_size / 10,
+                x: Math.floor(Math.random() * (jar_size*0.8 - bubble_size)),
+                y: Math.floor(Math.random() * (jar_size*0.8 - bubble_size)),
             }
             distX = ballPosition.x - centerPoint.x - bubble_size/2;
             distY = ballPosition.y - centerPoint.y - bubble_size/2;
-            
         }
+        
         return ballPosition;
     }
 
-    function calculateNewVelocity(ballPosition: Vector, ballRadius: number, centerPoint: Vector, circleRadius: number, velocity: Vector): Vector {
-        const normalVector = { x: ballPosition.x - centerPoint.x, y: ballPosition.y - centerPoint.y };
+    function calculateNewVelocity(ballPosition: Vector, centerPoint: Vector, velocity: Vector): Vector {
+        const normalVector = { x: ballPosition.x - centerPoint.x, y: ballPosition.y - centerPoint.y};
         const normalMagnitude = Math.sqrt(normalVector.x * normalVector.x + normalVector.y * normalVector.y);
         const normalizedNormalVector = { x: normalVector.x / normalMagnitude, y: normalVector.y / normalMagnitude };
         const dotProduct = velocity.x * normalizedNormalVector.x + velocity.y * normalizedNormalVector.y;
         const reflectedVelocity = { x: velocity.x - 2 * dotProduct * normalizedNormalVector.x, y: velocity.y - 2 * dotProduct * normalizedNormalVector.y };
         
-        return { x: parseFloat(reflectedVelocity.x.toFixed(4)), y: parseFloat(reflectedVelocity.y.toFixed(4))};
+        return reflectedVelocity;
     }
 
     function getClosestBallPositionOnCircle(
         ballPosition: Vector,
         ballVelocity: Vector,
         circle: Vector,
+        Radius: number,
         radius: number
-      ) {
-        // Calculate distance from center of the circle to current position of the ball
+    ) {
         const distance = Math.sqrt(
           Math.pow(ballPosition.x - circle.x, 2) + Math.pow(ballPosition.y - circle.y, 2)
         );
       
-        // Calculate the distance between the current position and intersection point
-        const distanceToIntersection = radius - distance;
+        const distanceToIntersection = distance - Radius + radius;
         
-        // Calculate the time it takes for the ball to reach the intersection point
         const timeToIntersection = distanceToIntersection / Math.sqrt(
           Math.pow(ballVelocity.x, 2) + Math.pow(ballVelocity.y, 2)
         );
       
-        // Calculate the new position of the ball at the intersection point
         const newBallPosition = {
-          x: ballPosition.x + ballVelocity.x * timeToIntersection,
-          y: ballPosition.y + ballVelocity.y * timeToIntersection,
+          x: ballVelocity.x * timeToIntersection,
+          y: ballVelocity.y * timeToIntersection,
         };
-      
         return newBallPosition;
-      }
-      
+    }      
 
     useEffect (() => {
         let jar = document.getElementById("jar") as HTMLDivElement;
-
         let bubbles = document.getElementsByClassName("randomColor") as HTMLCollectionOf<HTMLDivElement>;
-        let i = 0;
-        
-        const vel = [6, 3, -6, -3];
 
         Array.from(bubbles).forEach((bubble: HTMLDivElement) => {
             const jar_size = jar.offsetHeight;
             const bubble_size = bubble.offsetHeight;
-            const centerPoint = { x: jar_size/2, y: jar_size/2 }; 
+            const centerPoint = { x: jar_size / 2, y: jar_size / 2 }; 
 
             let position: Vector = randomSpawnPosition(centerPoint, jar_size, bubble_size);
             
-            bubble.style.top = "100" + "px";
-            bubble.style.left = "100" + "px" ;
-            i+=1;
-
+            bubble.style.left = position.x.toString() + "px" ;         
+            bubble.style.top = position.y.toString() + "px";           
+            const randomSign = Math.random() < 0.5 ? -1 : 1;
+            const speed = 30;
             let velocity = {
-                x:  1,
-                y:  1,
+                x:  Math.floor(Math.random() * (speed)) * randomSign,
+                y:  0,
             }
-            let j=0;
-            function moveBubble() {
-                j+=1;
-                let topPosition = parseFloat(bubble.style.top);
-                let leftPosition = parseFloat(bubble.style.left);       
+            velocity.y = Math.sqrt(Math.pow(speed, 2) - Math.pow(velocity.x, 2)) * -randomSign;
             
+            function moveBubble() {
+                let topPosition = parseFloat(bubble.style.top);
+                let leftPosition = parseFloat(bubble.style.left); 
+
+                leftPosition += velocity.x;
+                topPosition += velocity.y;
                 let ballPosition = { x: leftPosition + bubble_size/2, y: topPosition + bubble_size/2};
                  
                 const distX = ballPosition.x - centerPoint.x;
                 const distY = ballPosition.y - centerPoint.y;
 
                 if (Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2)) >= jar_size / 2 - bubble_size / 2) {
-                    let xd = getClosestBallPositionOnCircle(ballPosition, velocity, centerPoint, jar_size / 2);
-                    //console.log(getClosestBallPositionOnCircle(ballPosition, velocity, centerPoint, jar_size / 2), ballPosition, velocity);
+                    let fixedPosition = getClosestBallPositionOnCircle(ballPosition, velocity, centerPoint, jar_size / 2, bubble_size/2);
+                    
+                    leftPosition -= fixedPosition.x;
+                    topPosition -= fixedPosition.y;
                     const newVelocity = calculateNewVelocity(
-                        ballPosition, 
-                        bubble_size / 2, 
-                        centerPoint, 
-                        jar_size / 2, 
+                        ballPosition,  
+                        centerPoint,
                         velocity); 
-                    //leftPosition -= velocity.x/5;
-                    //topPosition -= velocity.y/5;
-                    console.log(j, leftPosition, topPosition, jar_size / 2 - bubble_size / 2, velocity, newVelocity, Math.sqrt(Math.pow(distX+newVelocity.x, 2) + Math.pow(distY+newVelocity.y, 2)));
-                    velocity = newVelocity;                 
+                   
+                    velocity = newVelocity;                
                 }
-                leftPosition += velocity.x;
-                topPosition += velocity.y;
-                
                 bubble.style.top = topPosition + 'px';
                 bubble.style.left = leftPosition + 'px';
 
@@ -161,7 +154,6 @@ const TechStackComponent = (() => {
                     requestAnimationFrame(moveBubble);
                 }, 1000/fps);
             }
-
             requestAnimationFrame(moveBubble);
         });
       }, []);
@@ -279,44 +271,129 @@ const TechStackComponent = (() => {
                                 <span></span>
                             </div>
                         </div>
-
-
-                        
                     </div>
-
-                    {/* <div className="jar-row"  id="jar-row-2">
+                    <div className="jar-row" id="jar-row-1">
                         <div className="bubble-container randomColor">
                             <div className="bubble">
                                 <span></span>
                                 <span></span>
                             </div>
                         </div>
-
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
                         <div className="bubble-container randomColor">
                             <div className="bubble">
                                 <span></span>
                                 <span></span>
                             </div>
                         </div>
-
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
                         <div className="bubble-container randomColor">
                             <div className="bubble">
                                 <span></span>
                                 <span></span>
                             </div>
                         </div>
-
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
                         <div className="bubble-container randomColor">
                             <div className="bubble">
                                 <span></span>
                                 <span></span>
-                                <div className="liquid"></div>
                             </div>
                         </div>
-                    </div> */}
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jar-row" id="jar-row-1">
+                        <div className="bubble-container randomColor">
+                            <div className="bubble">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
             <div className="x"></div>
             
         </div>
