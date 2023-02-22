28e7766367843b585330e3ee74f22034ba3bdf0e
himok <himok.pk@gmail.com>
2023-02-22 22:56:06 +0100

    Added stack hover effect

--- ui/src/components/NavBar.tsx
+++ ui/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import debounce from 'lodash/debounce';
 
