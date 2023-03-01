f3b23be110dcee0eca6758e24d3f14a8792ad178
himok <himok.pk@gmail.com>
2023-03-01 17:11:56 +0100

    Adding missing files

---
+++ ui/src/components/FooterComponent.tsx
@@ -0,0 +1,40 @@
+import { AiFillGithub } from "react-icons/ai";
+import { CiMail } from "react-icons/ci";
+import { FaCity } from "react-icons/fa";
+import { IoIosPhonePortrait } from "react-icons/io";
+
+import EmailSender from "./EmailSenderComponent";
+
+import "../css/footer.css";
+
+const FooterComponent = () => {
+    const github_url = "https://github.com/himokkk/";
+    const linkedin_url = "https://www.linkedin.com/in/pawe%C5%82-kwieci%C5%84ski-3572881b7/";
+    const slack_url = "https://slack.com/";
+
+    return (
+        <div id="contact">
+            <div className="contact-content">
+                <EmailSender />
+                <div className="contact-row">
+                    <div className="info">
+                        <FaCity /> Warsaw
+                    </div>
+                    <div className="info">
+                        <IoIosPhonePortrait /> +48 665 341 306
+                    </div>
+                    <div className="info">
+                        <CiMail /> pawel.kwiecinski@student.uksw.edu.pl
+                    </div>
+                    <div className="info">
+                        <a href={github_url} target="_blank">
+                            <AiFillGithub /> github.com/himokkk
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FooterComponent;
---
+++ ui/src/components/TimeLineComponent.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import "../css/timeline.css";
+
+const TimeLineComponent = () => {
+    return (
+        <div className="timeline-main" id="timeline">
+            <div className="techstack-header">
+                <h1>Experience</h1>
+                <h1>Education</h1>
+            </div>
+            <div className="timeline-content">
+                <div className="left-container">
+                    <div className="timestamp" id="comp-timestamp">
+                        <div className="timestamp-content">
+                            <div className="timestamp-date">07-09.2022</div>
+                            <div className="timestamp-title">Backend Developer | Comp S. A.</div>
+                            <div className="timestamp-description">
+                                Warsaw | Technologies used: <br />
+                                Django, Django Rest Framework, Docker, Docker-compose, React(JavaScript, TypeScript),
+                                HTML, CSS, Poetry, CD/CI.
+                            </div>
+                        </div>
+                        <div className="timestamp-hr" />
+                    </div>
+                </div>
+                <hr id="timeline-hr" />
+                <div className="right-container">
+                    <div className="timestamp" id="high-school-timestamp">
+                        <div className="timestamp-hr" />
+                        <div className="timestamp-content">
+                            <div className="timestamp-date">2018-2021</div>
+                            <div className="timestamp-title">CLX High School</div>
+                            <div className="timestamp-description">Mathematics and Computer Science</div>
+                        </div>
+                    </div>
+
+                    <div className="timestamp" id="studies-timestamp">
+                        <div className="timestamp-hr" />
+                        <div className="timestamp-content">
+                            <div className="timestamp-date">2021-2024</div>
+                            <div className="timestamp-title">UKSW</div>
+                            <div className="timestamp-description">Warsaw | BS Computer Science</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TimeLineComponent;
